bebd980a635d9e7778e0d16368b86300f72ac72d
guriOH <>
2020-06-24 00:03:16 +0900

    Add applymiddleware redux thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
 
 import App from "./Components/App";
 import reducers from "./Reducers";
 
+
 import "./index.css";
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.querySelector("#root")
